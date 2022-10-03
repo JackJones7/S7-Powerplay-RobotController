f7e9c01107f9f772cdd75a2ecd70ee175f41e9c5
Jack Jones <jackij43@outlook.com>
2022-10-02 22:37:31 -0500

    fixed some merge problems (whoops)

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveByGyro_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveByGyro_Linear.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* Copyright (c) 2017 FIRST. All rights reserved.
 =======
 /* Copyright (c) 2022 FIRST. All rights reserved.
@@ -33,11 +33,9 @@
 
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-<<<<<<< HEAD
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
-=======
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
->>>>>>> roadrunner-update
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -45,30 +43,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-<<<<<<< HEAD
-/**
- * This file illustrates the concept of driving a path based on Gyro heading and encoder counts.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByTime;
- *
- *  This code ALSO requires that you have a Modern Robotics I2C gyro with the name "gyro"
- *   otherwise you would use: RobotAutoDriveByEncoder;
- *
- *  This code requires that the drive Motors have been configured such that a positive
- *  power command moves them forward, and causes the encoders to count UP.
- *
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- *  In order to calibrate the Gyro correctly, the robot must remain stationary during calibration.
- *  This is performed when the INIT button is pressed on the Driver Station.
- *  This code assumes that the robot is stationary when the INIT button is pressed.
- *  If this is not the case, then the INIT should be performed again.
- *
- *  Note: in this example, all angles are referenced to the initial coordinate frame set during the
- *  the Gyro Calibration process, or whenever the program issues a resetZAxisIntegrator() call on the Gyro.
-=======
+import org.checkerframework.checker.units.qual.Speed;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -100,17 +81,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  *  All angles are referenced to the coordinate-frame that is set whenever resetHeading() is called.
  *  In this sample, the heading is reset when the Start button is touched on the Driver station.
  *  Note: It would be possible to reset the heading after each move, but this would accumulate steering errors.
->>>>>>> roadrunner-update
  *
  *  The angle of movement/rotation is assumed to be a standardized rotation around the robot Z axis,
  *  which means that a Positive rotation is Counter Clockwise, looking down on the field.
  *  This is consistent with the FTC field coordinate conventions set out in the document:
  *  ftc_app\doc\tutorial\FTC_FieldCoordinateSystemDefinition.pdf
  *
-<<<<<<< HEAD
- *  Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- *  Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
-=======
+
  *  Control Approach.
  *
  *  To reach, or maintain a required heading, this code implements a basic Proportional Controller where:
@@ -124,7 +101,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  *
  *  Use Android Studio to Copy this Class, and Paste it into your "TeamCode" folder with a new name.
  *  Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
->>>>>>> roadrunner-update
+
  */
 
 @Autonomous(name="Robot: Auto Drive By Gyro", group="Robot")
@@ -240,7 +217,7 @@ public class RobotAutoDriveByGyro_Linear extends LinearOpMode {
         sleep(1000);  // Pause to display last telemetry message.
     }
 
-
+    /*
     * @param maxDriveSpeed MAX Speed for forward/rev motion (range 0 to +1.0) .
     * @param distance   Distance (in inches) to move from current position.  Negative distance means move backward.
     * @param heading      Absolute Heading Angle (in Degrees) relative to last gyro reset.

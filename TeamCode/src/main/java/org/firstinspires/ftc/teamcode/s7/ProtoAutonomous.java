364a4049a58feb76ea9aac3cb7bdf42c1f5850e4
Jack Jones <jackij43@outlook.com>
2022-10-09 17:37:13 -0500

    added code to store signal image

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/ProtoAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/ProtoAutonomous.java
@@ -11,45 +11,51 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
+import org.firstinspires.ftc.teamcode.s7.framework.tfHandler;
+
 import java.util.List;
 
 @Autonomous
 public class ProtoAutonomous extends OpMode{
 
     /*
-    * Constants
+    * Variables and Constants
     */
 
     /* Vuforia */
 
-    //Vufoira license key
-    private static final String VUFORIA_KEY =
+    private static final String VUFORIA_KEY =    //Vufoira license key
             "AUi0aKj/////AAABmXTlGAMS7ElAl7wgWIxVKWxeoxIvuQjEO8bL3NSZa1QAvP6uPT1n2wcNinINBfUHvxS7W3cNP2Z7SMmFvuZnUlfv4zZ7it6zz1A/KALMuKQ0/64zdM2AsJH7TotFBlg3EhyNzKcTvw/tclxaO8AiQualjV2g1wmiyIjosLF5zEKVfEQKRlf13RJZX1F9qElreZv+SJ6guswbv50k8ZiwOKLGtSGtzNxgf9GCLHDwt1WUTK6a+upJPJA9uddiC9gPoqLTc6EsDMQ81NWvKLjk8YHQxVZUTkFYpV6RfmABwhkudAWbIcv9voWA2tVAGDTmLZKwo1U8w+WBDvSOZdZCDowy0e58SBhT33g0o1m0HKCz";
 
-    //Instance of vuforia localizer
-    private VuforiaLocalizer vuforia;
+    private VuforiaLocalizer vuforia;    //Instance of vuforia localizer
+
 
 
     /* TensorFlow */
 
-    //Path to TensorFlow model
-    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite"; //Image recognition model
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";      //Path to TensorFlow model
 
-    //TensorFlow object detection engine instance
-    private TFObjectDetector tfod;
+    private TFObjectDetector tfod;   //TensorFlow object detection engine instance
 
-    //Tensor flow labels
-    private static final String[] LABELS = {
+    private static final String[] LABELS = {    //Tensor flow labels
             "1 Bolt",
             "2 Bulb",
             "3 Panel"
     };
 
 
+
+    //Core
+
+    //TODO: Change this to enum or other better type
+    private String signalFront = ""; //Stores image on front of signal, determined on loop()
+
+
+
     @Override
     public void init() {
         //TF relies on Vuforia, so init Vuforia first
-        initVuforia();
+        vuforia = tfHandler.initVuforia(VUFORIA_KEY);
         initTfod();
 
         //If TF was successfully initialized
@@ -73,29 +79,28 @@ public class ProtoAutonomous extends OpMode{
 
     @Override
     public void loop() {
-        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions(); //Get new recognitions, if any
 
-        if (updatedRecognitions != null) {
-            for (Recognition recognition : updatedRecognitions) {
-                telemetry.addData("Image", recognition.getLabel());
-                telemetry.addData("confidence", recognition.getConfidence());
+
+        findSignalFront();
+        telemetry.addData("Signal front", signalFront);
+        telemetry.update();
+
+
+    }
+
+
+    private void findSignalFront() {
+        if (signalFront == "") {     //If he haven't found the signal front, try again
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions(); //Get new recognitions, if any
+
+            if (updatedRecognitions != null) {
+                signalFront = updatedRecognitions.get(0).getLabel();
             }
         }
     }
 
 
-
-    //TODO: Move these init functions to static class for abstraction
-    private void initVuforia() {
-        //Create parameters to pass to vuforia
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-    }
+    //TODO: Move TF Init to tfHandler
 
     private void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/tfHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/tfHandler.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.s7.framework;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
 public class tfHandler {
+
+    public static VuforiaLocalizer initVuforia(String VUFORIA_KEY) {
+
+        VuforiaLocalizer vuforia;
+
+        //Create parameters to pass to vuforia
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        return vuforia;
+    }
+
 }

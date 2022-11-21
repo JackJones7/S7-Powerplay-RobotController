a61cf0e63c8c70e11931f87e028adef7d8a1ef7d
Jack Jones <jackij43@outlook.com>
2022-11-20 23:55:51 -0600

    pleeeeease

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/ProtoAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/ProtoAutonomous.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.s7;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -16,26 +17,11 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import java.util.List;
 
 @Autonomous
-public class ProtoAutonomous extends OpMode{
-
-    /*
-    * Variables and Constants
-    */
-
-    /* Vuforia */
+public class ProtoAutonomous extends LinearOpMode {
 
     private static final String VUFORIA_KEY =    //Vufoira license key
             "AUi0aKj/////AAABmXTlGAMS7ElAl7wgWIxVKWxeoxIvuQjEO8bL3NSZa1QAvP6uPT1n2wcNinINBfUHvxS7W3cNP2Z7SMmFvuZnUlfv4zZ7it6zz1A/KALMuKQ0/64zdM2AsJH7TotFBlg3EhyNzKcTvw/tclxaO8AiQualjV2g1wmiyIjosLF5zEKVfEQKRlf13RJZX1F9qElreZv+SJ6guswbv50k8ZiwOKLGtSGtzNxgf9GCLHDwt1WUTK6a+upJPJA9uddiC9gPoqLTc6EsDMQ81NWvKLjk8YHQxVZUTkFYpV6RfmABwhkudAWbIcv9voWA2tVAGDTmLZKwo1U8w+WBDvSOZdZCDowy0e58SBhT33g0o1m0HKCz";
-
-    private VuforiaLocalizer vuforia;    //Instance of vuforia localizer
-
-
-
-    /* TensorFlow */
-
     private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";      //Path to TensorFlow model
-
-    private TFObjectDetector tfod;   //TensorFlow object detection engine instance
 
     private static final String[] LABELS = {    //Tensor flow labels
             "1 Bolt",
@@ -44,108 +30,97 @@ public class ProtoAutonomous extends OpMode{
     };
 
 
-
-    /* Core */
-
-    private int jobIndex = 0;
+    private VuforiaLocalizer vuforia;    //Instance of vuforia localizer
+    private TFObjectDetector tfod;   //TensorFlow object detection engine instance
+    private SampleMecanumDrive drive;
 
     //TODO: Change this to enum or other better type
     private String signalFront = ""; //Stores image on front of signal, determined on loop()
 
-    private SampleMecanumDrive drive;
 
-    private double inwardDir = -1;
+    public void runOpMode() throws InterruptedException {
 
-    /* A ton of trajectories */
+        /*
+        ************************Init**********************
+        */
 
-    Trajectory trForward;
-    Trajectory trOutward1Tile;
-    Trajectory trOutward;
-    Trajectory trForward3Tiles;
-
-    private TrajectorySequence currentSequence;
-
-
-
-    @Override
-    public void init() {
-        //TF relies on Vuforia, so init Vuforia first
-        vuforia = tfHandler.initVuforia(VUFORIA_KEY);
-        tfod = tfHandler.initTfod(hardwareMap, vuforia, TFOD_MODEL_ASSET, LABELS);
         drive = new SampleMecanumDrive(hardwareMap);
 
+        vuforia = tfHandler.initVuforia(VUFORIA_KEY);
+        tfod = tfHandler.initTfod(hardwareMap, vuforia, TFOD_MODEL_ASSET, LABELS);
         //If TF was successfully initialized
         if (tfod != null) {
             tfod.activate();
 
             //If problems arise, try messing with the zoom. see ConceptTensorFlowObjectDetection line 113
         }
-        //TODO: Add Telemetry if TF fails
-
         telemetry.addData("Tensorflow", "Ready");
         telemetry.update();
+        //TODO: Add Telemetry if TF fails
 
-        buildTrajectories();
-    }
+        Pose2d startPose = new Pose2d(-34.0, 48, 90.0);
+        drive.setPoseEstimate(startPose);
 
-    /*
-    @Override
-    public void start() {
+        waitForStart();
 
-    }
-    */
+        /*
+        ****************************Run****************************
+        */
 
-    @Override
-    public void loop() {
+        if (isStopRequested()) return;
 
-        //I guess this is a state machine
-        switch(jobIndex) {
+        findSignalFront();
+        while (!isStopRequested() && signalFront == "") {
+            findSignalFront();
+            telemetry.addData("Recognition", "waiting...");
+            telemetry.update();
+        }
+        telemetry.addData("Recognition", "found" + signalFront);
+        telemetry.update();
 
-            case 0:
-                findSignalFront();
-                telemetry.addData("Signal front", signalFront);
-                telemetry.update();
-
-                if (signalFront != "") {
-                    jobIndex++;
-                }
-                jobIndex++;
-                break;
-
-            case 1:
-
-                currentSequence = drive.trajectorySequenceBuilder(new Pose2d())
-                        .strafeRight(24.0)
-                        .forward(72.0)
-                        .build();
-
-                break;
-
-            //case 2:
-            //    drive.followTrajectory(trForward3Tiles);
-//
-            //    if(!drive.isBusy()) {
-            //        jobIndex++;
-            //    }
-//
-            //    break;
-
+        drive.followTrajectorySequence(drive.trajectorySequenceBuilder(startPose)
+                .strafeRight(24.0)
+                .forward(50.5)
+                .turn(Math.toRadians(90.0))
+                .build());
+        while (!isStopRequested() && drive.isBusy()) {
+            idle();
         }
 
-        if (currentSequence != null) {
-            drive.followTrajectorySequence(currentSequence);
+        //Placeholder for code to grab cone
+
+        drive.followTrajectorySequence(drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                .forward(48.0)
+                .strafeLeft(12.0)
+                .build());
+        while (!isStopRequested() && drive.isBusy()) {
+            idle();
         }
 
-        if (!drive.isBusy()) {
-            currentSequence = null;
+        //Placeholder for code to place cone
+
+        drive.followTrajectorySequence((drive.trajectorySequenceBuilder(drive.getPoseEstimate()))
+                .strafeRight(12.0)
+                .back(48.0)
+                .build());
+        while(!isStopRequested() && drive.isBusy()) {
+            idle();
         }
 
+        //Placeholder for code to grab cone
 
+        drive.followTrajectorySequence(drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                .forward(48.0)
+                .strafeLeft(12.0)
+                .build());
+        while (!isStopRequested() && drive.isBusy()) {
+            idle();
+        }
 
-
-
+        //Placeholder for code to place cone
 
     }
+
 
 
     private void findSignalFront() {
@@ -166,23 +141,5 @@ public class ProtoAutonomous extends OpMode{
         }
     }
 
-
-    private void buildTrajectories() {
-        trForward = drive.trajectoryBuilder(new Pose2d())
-                .forward(12.0)
-                .build();
-
-        trOutward1Tile = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(24.0)
-                .build();
-
-        trOutward = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(24.0)
-                .build();
-
-        trForward3Tiles = drive.trajectoryBuilder(new Pose2d())
-                .forward(72.0)
-                .build();
-    }
 
 }

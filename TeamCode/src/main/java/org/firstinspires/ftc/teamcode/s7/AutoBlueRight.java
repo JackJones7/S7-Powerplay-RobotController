bd3620bb617e2d1534de9c042fef3ed86f2fc244
Jack Jones <jackij43@outlook.com>
2022-12-11 23:52:14 -0600

    tuned roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/AutoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/AutoBlueRight.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.s7;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.s7.framework.S7Robot;
 
+@Autonomous
 public class AutoBlueRight extends LinearOpMode {
 
     protected S7Robot robot;

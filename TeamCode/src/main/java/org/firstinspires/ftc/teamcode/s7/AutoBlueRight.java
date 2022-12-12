81b78d238a2c4fd11201b630f0cd111b18d5d33b
Jack Jones <jackij43@outlook.com>
2022-12-11 23:27:12 -0600

    added new autonomous with test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/AutoBlueRight.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.s7;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.s7.framework.S7Robot;
+
+public class AutoBlueRight extends LinearOpMode {
+
+    protected S7Robot robot;
+
+    public void runOpMode() throws InterruptedException {
+
+        /*               Init                       */
+        robot = new S7Robot(hardwareMap, "LiftMotor", "ClawLeft", "ClawRight");
+
+        waitForStart();
+        /*               Run                       */
+
+        robot.forward(24);
+        robot.waitForRR(this);
+        robot.back(24);
+        robot.waitForRR(this);
+    }
+
+}

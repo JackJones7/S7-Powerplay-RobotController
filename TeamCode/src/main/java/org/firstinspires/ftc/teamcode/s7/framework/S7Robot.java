df1bc34c20382d96f81d4d114e55246103b17e44
Jack Jones <jackij43@outlook.com>
2022-12-11 22:49:32 -0600

    psuedo code for robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/S7Robot.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.s7.framework;
+
+public class S7Robot {
+
+    //hardware map
+    //sample mechanum drive
+    //arm elevation motor
+    //left grabber servo
+    //right grabber servo
+
+    /*constructor: takes a hardware map and names for arm motor and grabber servos.
+    * gets motor and servos from hardware map
+    * initializes elevation motor for runToPosition mode
+    * */
+
+    /*constructor: same as above + takes start pose, sets drive's pose */
+
+
+    /*Move forward: Takes move distance, uses RR to move forward that distance from pose estimate*/
+
+    /*Move back: Takes move distance, uses RR to move backward that distance from pose estimate*/
+
+    /*Strafe Right: Takes move distance, uses RR to strafe right that distance from pose estimate*/
+
+    /*Strafe Left: Takes move distance, uses RR to strafe left that distance from pose estimate*/
+
+    /*Set Arm Position: Sets arm motor to RunToPosition mode if necessary, set arm motor's position*/
+
+    /*Power Arm: Set arm motor to RunWithoutEncoder if necessary, set arm motor's power*/
+
+    /*Set grabber position: Set position of left and right servos. 0 is closed, 1 is open*/
+}

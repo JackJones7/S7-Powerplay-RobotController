27b60ef02f77fd0d05f2a49f80e630f0c56857c8
Jack Jones <jackij43@outlook.com>
2022-12-11 23:11:40 -0600

    added code to robot class that will definitely work on the first try

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/S7Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/S7Robot.java
@@ -1,32 +1,104 @@
 package org.firstinspires.ftc.teamcode.s7.framework;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 public class S7Robot {
 
     //hardware map
+    private HardwareMap hardwareMap;
     //sample mechanum drive
+    private SampleMecanumDrive drive;
     //arm elevation motor
+    private DcMotor armMotor;
     //left grabber servo
+    private Servo grabberLeft;
     //right grabber servo
+    private Servo grabberRight;
 
     /*constructor: takes a hardware map and names for arm motor and grabber servos.
     * gets motor and servos from hardware map
+    * initializes drive
     * initializes elevation motor for runToPosition mode
     * */
+    public S7Robot(HardwareMap hardwareMap, String armMotorName, String grabberLeftName, String grabberRightName) {
+        this.hardwareMap = hardwareMap;
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        this.armMotor = hardwareMap.get(DcMotor.class, armMotorName);
+        this.grabberLeft = hardwareMap.get(Servo.class, grabberLeftName);
+        this.grabberRight = hardwareMap.get(Servo.class, grabberRightName);
+
+        this.grabberLeft.setDirection(Servo.Direction.REVERSE);
+
+        this.armMotor.setTargetPosition(0);
+    }
 
     /*constructor: same as above + takes start pose, sets drive's pose */
 
 
     /*Move forward: Takes move distance, uses RR to move forward that distance from pose estimate*/
+    public void forward(double distance) {
+        drive.followTrajectory(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .forward(distance)
+                .build());
+    }
 
     /*Move back: Takes move distance, uses RR to move backward that distance from pose estimate*/
+    public void back(double distance) {
+        drive.followTrajectory(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .back(distance)
+                .build());
+    }
 
     /*Strafe Right: Takes move distance, uses RR to strafe right that distance from pose estimate*/
+    public void strafeRight(double distance) {
+        drive.followTrajectory(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .strafeRight(distance)
+                .build());
+    }
 
     /*Strafe Left: Takes move distance, uses RR to strafe left that distance from pose estimate*/
+    public void strafeLeft(double distance) {
+        drive.followTrajectory(drive.trajectoryBuilder(drive.getPoseEstimate())
+                .strafeLeft(distance)
+                .build());
+    }
+
+    /*Turn: takes angle, use RR to turn at that angle*/
+    public void turn(double angle) {
+        drive.turn(Math.toRadians(angle));
+    }
 
     /*Set Arm Position: Sets arm motor to RunToPosition mode if necessary, set arm motor's position*/
+    public void setArmPosition(int position) {
+        if (armMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        armMotor.setTargetPosition(position);
+    }
 
     /*Power Arm: Set arm motor to RunWithoutEncoder if necessary, set arm motor's power*/
+    public void setArmPower(double power) {
+        if (armMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
+            armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+
+        armMotor.setPower(power);
+    }
 
     /*Set grabber position: Set position of left and right servos. 0 is closed, 1 is open*/
+    public void setGrabberPosition(double position) {
+        grabberRight.setPosition(position);
+        grabberLeft.setPosition(position);
+    }
+
+    /*IsBusy: return drive's IsBusy*/
+    public boolean isBusy() {
+        return drive.isBusy();
+    }
 }

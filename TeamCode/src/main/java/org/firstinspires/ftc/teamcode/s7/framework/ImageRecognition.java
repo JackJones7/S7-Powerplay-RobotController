eaf2826cacf7862c380b510f2970047f868bef21
Jack Jones <jackij43@outlook.com>
2022-12-19 00:15:29 -0600

    working on encapsulating ImageRecognition class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.s7.framework;
 
 import com.vuforia.Vuforia;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
@@ -9,7 +10,7 @@ public class ImageRecognition {
 
     private static final String VUFORIA_KEY =
             "AUi0aKj/////AAABmXTlGAMS7ElAl7wgWIxVKWxeoxIvuQjEO8bL3NSZa1QAvP6uPT1n2wcNinINBfUHvxS7W3cNP2Z7SMmFvuZnUlfv4zZ7it6zz1A/KALMuKQ0/64zdM2AsJH7TotFBlg3EhyNzKcTvw/tclxaO8AiQualjV2g1wmiyIjosLF5zEKVfEQKRlf13RJZX1F9qElreZv+SJ6guswbv50k8ZiwOKLGtSGtzNxgf9GCLHDwt1WUTK6a+upJPJA9uddiC9gPoqLTc6EsDMQ81NWvKLjk8YHQxVZUTkFYpV6RfmABwhkudAWbIcv9voWA2tVAGDTmLZKwo1U8w+WBDvSOZdZCDowy0e58SBhT33g0o1m0HKCz";
-    private static final String TFOD_ASSET = "PowerPlay.tflite";
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
     private static final String[] LABELS = {
             "1 Bolt",
             "2 Bulb",
@@ -20,7 +21,19 @@ public class ImageRecognition {
     private TFObjectDetector tfod;
 
     public ImageRecognition () {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.fillCameraMonitorViewParent = true;
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters();
+        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 300;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+        tfod.activate();
     }
 
 }

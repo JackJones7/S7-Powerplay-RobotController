744af8f8032703faee30cd3c94c1eae4d7bdbb18
Jack Jones <jackij43@outlook.com>
2023-01-06 16:48:54 -0600

    finished ImageRecognition class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
@@ -4,21 +4,25 @@ import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import java.util.List;
 
 public class ImageRecognition {
 
     private static final String VUFORIA_KEY =
             "AUi0aKj/////AAABmXTlGAMS7ElAl7wgWIxVKWxeoxIvuQjEO8bL3NSZa1QAvP6uPT1n2wcNinINBfUHvxS7W3cNP2Z7SMmFvuZnUlfv4zZ7it6zz1A/KALMuKQ0/64zdM2AsJH7TotFBlg3EhyNzKcTvw/tclxaO8AiQualjV2g1wmiyIjosLF5zEKVfEQKRlf13RJZX1F9qElreZv+SJ6guswbv50k8ZiwOKLGtSGtzNxgf9GCLHDwt1WUTK6a+upJPJA9uddiC9gPoqLTc6EsDMQ81NWvKLjk8YHQxVZUTkFYpV6RfmABwhkudAWbIcv9voWA2tVAGDTmLZKwo1U8w+WBDvSOZdZCDowy0e58SBhT33g0o1m0HKCz";
     private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
-    private static final String[] LABELS = {
+
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    public static final String[] LABELS = {
             "1 Bolt",
             "2 Bulb",
             "3 Panel"
     };
 
-    private VuforiaLocalizer vuforia;
-    private TFObjectDetector tfod;
 
     public ImageRecognition () {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
@@ -35,5 +39,32 @@ public class ImageRecognition {
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
         tfod.activate();
     }
+
+    public List<Recognition> getRecognitions() {
+        return tfod.getRecognitions();
+    }
+
+    public List<Recognition> getUpdatedRecognitions() {
+        return tfod.getUpdatedRecognitions();
+    }
+
+    public String detectSignal() {
+        List<Recognition> updatedRecognitions = getUpdatedRecognitions();
+        if (updatedRecognitions == null) {return "";}
+
+        double bestConfidence = 0.0;
+        String label = "";
+
+        for (Recognition recognition : updatedRecognitions) {
+            if (recognition.getConfidence() > bestConfidence) {
+                label = recognition.getLabel();
+                bestConfidence = recognition.getConfidence();
+            }
+        }
+
+        return label;
+    }
+
+
 
 }

e87b3228bf69b7a3d2f8024d835d8711eb5ce5c6
Jack Jones <jackij43@outlook.com>
2023-02-14 23:13:31 -0600

    change angular accel and angular vel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -67,7 +67,7 @@ public class DriveConstants {
     public static double MAX_VEL = 60;
     public static double MAX_ACCEL = 15;
     public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(50);
+    public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
     * int accelerate(int sSpeed, int accel, int eSpeed){

cd40e3fb889afa2f821c1324dfeef96dab5cfdab
Jack Jones <jackij43@outlook.com>
2023-02-24 17:57:06 -0600

    use new tf model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/s7/framework/ImageRecognition.java
@@ -12,7 +12,7 @@ public class ImageRecognition {
 
     private static final String VUFORIA_KEY =
             "AUi0aKj/////AAABmXTlGAMS7ElAl7wgWIxVKWxeoxIvuQjEO8bL3NSZa1QAvP6uPT1n2wcNinINBfUHvxS7W3cNP2Z7SMmFvuZnUlfv4zZ7it6zz1A/KALMuKQ0/64zdM2AsJH7TotFBlg3EhyNzKcTvw/tclxaO8AiQualjV2g1wmiyIjosLF5zEKVfEQKRlf13RJZX1F9qElreZv+SJ6guswbv50k8ZiwOKLGtSGtzNxgf9GCLHDwt1WUTK6a+upJPJA9uddiC9gPoqLTc6EsDMQ81NWvKLjk8YHQxVZUTkFYpV6RfmABwhkudAWbIcv9voWA2tVAGDTmLZKwo1U8w+WBDvSOZdZCDowy0e58SBhT33g0o1m0HKCz";
-    private static final String TFOD_MODEL_ASSET = "ShapeSignal.tflite";
+    private static final String TFOD_MODEL_ASSET = "ShapeSignal2.tflite";
 
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
